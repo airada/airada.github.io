46bb8b2ea06b524872fa3c4ee89aa7c1e20e9f20
airada <jasmineirada@gmail.com>
2020-08-02 11:11:47 -0700

    Added Filtering functionality for projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  skills: any = ["C#", "C++", "PYTHON", "JAVA", "GIT", "MYSQL", "UNITY", "UNREAL", "BLENDER"];
+  skills: any = ["C#", "C++", "PYTHON", "JAVA", "GIT", "MYSQL", "UNITY", "UNREAL", "BLENDER", "DESIGN", "BACKEND", "MOBILE", "VIRTUAL REALITY"];
+  chosen_skill: any = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  filterSkill(skill_item){
+    if (this.chosen_skill.includes(skill_item)){
+      this.chosen_skill.splice(this.chosen_skill.indexOf(skill_item), 1);
+      console.log(skill_item + " was UNSELECTED");
+    } else {
+      this.chosen_skill.push(skill_item);
+      console.log(skill_item + " was SELECTED");
+    }
+
   }
 
 }
