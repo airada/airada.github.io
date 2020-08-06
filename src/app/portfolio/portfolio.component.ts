a8501d6d42681e0f67d3ddcec377732ffc664802
airada <jasmineirada@gmail.com>
2020-08-05 18:19:35 -0700

    Using CSS to hide elements. Single tag filter is working, need to add multiple filters function

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,32 +6,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  skills: any = ["C#", "C++", "PYTHON", "JAVA", "GIT", "MYSQL", "UNITY", "UNREAL", "BLENDER", "DESIGN", "BACKEND", "MOBILE", "VIRTUAL REALITY"];
+  skill_tags: any = [["cs","C#"], ["cpp","C++"], ["python","PYTHON"],["java","JAVA"], ["unity","UNITY"],["unreal","UNREAL"],["design","DESIGN"], ["backend","BACKEND"], ["mobile","MOBILE"], ["vr","VIRTUAL REALITY"]];
   chosen_skill: any = [];
+  skill_array: any = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  filterSkill(skill_item){
+  ngAfterViewInit(){
+    // you'll get your through 'elements' below code
+}
+
+  //Adds or Removes Skill to filtering list
+  add_skill(skill_item){
 
     if (this.chosen_skill.includes(skill_item)){
       this.chosen_skill.splice(this.chosen_skill.indexOf(skill_item), 1);
-      console.log(skill_item + " was UNSELECTED");
-      var inputValue = (<HTMLInputElement>document.getElementById(skill_item)); 
-      inputValue.classList.remove('bg-warning');  
+      //console.log(skill_item + " was UNSELECTED");
+      this.change_btn_color(skill_item, 'bg-warning','bg-dark');
 
-      inputValue.classList.add('bg-dark');
-      
     } else {
       this.chosen_skill.push(skill_item);
-      console.log(skill_item + " was SELECTED");
-      var inputValue = (<HTMLInputElement>document.getElementById(skill_item)); 
-      inputValue.classList.remove('bg-dark');  
+      //console.log(skill_item + " was SELECTED");
+      this.change_btn_color(skill_item, 'bg-dark','bg-warning');
+      this.filter_skill();
+    }
 
-      inputValue.classList.add('bg-warning');
+    console.log(this.chosen_skill);
+  }
+
+  change_btn_color(item, removed, added){
+    var inputValue = (<HTMLInputElement>document.getElementById(item));
+    inputValue.classList.remove(removed);  
+    inputValue.classList.add(added);
+  }
+
+  is_chosen(skill){
+    return this.chosen_skill.includes(skill)
+  }
+  
+  // Looks for components with ALL skills NOT chosen 
+  // 1. Loop through skill tags to find which HTML elements DO NOT have the chosen skill in order to HIDE them
+  // 2. SHOW HTML elements that have the class
+  filter_skill(){
+    for(let i = 0; i < this.skill_tags.length; i++){
+      this.hide_class(this.skill_tags[i][0]);     //initially hides all HTML elements
+    }
+
+    for(let j = 0; j < this.chosen_skill.length; j++){
+      this.toggle_class(this.chosen_skill[j]);
     }
   }
+
+  hide_class(skill){
+    let elements = document.querySelectorAll("."+skill);
+    elements.forEach(element => {
+      element.classList.add('hidden');
+    });
+  }
+
+//Toggles hidden class
+  toggle_class(skill){
+    console.log(skill);
+    let elements = document.querySelectorAll("."+skill);
+    elements.forEach(element => {
+      element.classList.toggle('hidden');
+      console.log(element.classList);
+    });
+  }
+
+ 
+
+
+
+  
 
 }
