714d64bf89a664a9a85dc1c4a0d8e9a8ebe3e537
airada <jasmineirada@gmail.com>
2021-11-11 13:31:31 -0800

    redesigned header, changed colors for less contrast for accessibility, changed filter style, pending astrokitty image

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -47,12 +47,13 @@ export class AboutComponent implements OnInit {
   private stars1: string;
   private stars2: string;
   public state = 'down';
+  private colors: Array<string> = ["#3C4F6C75", "#1A4C4A75", "#FED3B7", "#FF925E80", "#FF7A66", "#ABA3D275", "#FFD9EE50", "#FFD96C", "#D65F9150"]
 
 
   ngOnInit() {
-    this.stars = this.box_shadow(400, 250);
-    this.stars1 = this.box_shadow(125, 200);
-    this.stars2 = this.box_shadow(50, 200);
+    this.stars = this.box_shadow(400, 250, 7, 8);
+    this.stars1 = this.box_shadow(200, 200, 2, 6);
+    this.stars2 = this.box_shadow(75, 200, 0, 1);
     document.getElementById("stars").style.boxShadow = this.stars;
     document.getElementById("stars1").style.boxShadow = this.stars1;
     document.getElementById("stars2").style.boxShadow = this.stars2;
@@ -82,11 +83,11 @@ export class AboutComponent implements OnInit {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  private box_shadow(num_stars, vh_height) {
+  private box_shadow(num_stars, vh_height, color1, color2) {
     let box_shadow_str = "";
 
     for (let i = 0; i <= num_stars; i++) {
-      box_shadow_str += this.rng(0, 100).toString() + "vw " + this.rng(0, vh_height).toString() + "vh " + "white"
+      box_shadow_str += this.rng(0, 100).toString() + "vw " + this.rng(0, vh_height).toString() + "vh " + "#faf9f7"
 
       if (i != num_stars) {
         box_shadow_str += ","
