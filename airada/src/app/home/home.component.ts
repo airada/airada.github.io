2b13aa978ba4bcb679185e1fb1af0af5aa9e1654
airada <23159600+airada@users.noreply.github.com>
2023-03-21 01:05:44 -0700

    experimenting with the home page design and theme

--- airada/src/app/home/home.component.ts
+++ airada/src/app/home/home.component.ts
@@ -6,10 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+ role: string = "> ";
+ typingSpeed: number = 100;
 
+  constructor(){
+  }
 
-  changeBG() {
-    var element = document.getElementById("header")!;
-    element.classList.toggle("bg-image-1");
+  ngOnInit(){
+
+  }
+
+  ngAfterViewInit() {
+    this.typeWriter("SOFTWARE ENGINEERING");
+    this.clear(this.delay("SOFTWARE ENGINEERING"));
+    this.typeWriter("GAME DEVELOPMENT", this.delay("SOFTWARE ENGINEERING"));
+    this.clear(this.delay("SOFTWARE ENGINEERING") + this.delay("GAME DEVELOPMENT"));
+    this.typeWriter("ART AND DESIGN", this.delay("SOFTWARE ENGINEERING") + this.delay("GAME DEVELOPMENT"));
+  }
+  
+
+ typeWriter(string: string, delay: number = 0) {
+    setTimeout(() => {
+        for (let i = 0; i < string.length; i++) {
+          this.type(string, this.typingSpeed * i, i);
+      }
+    }, delay);
+  }
+
+  type(string: string, typingSpeed: number, index: number) {
+      setTimeout(() => {
+        document.getElementById("typewriter")!.innerHTML += string.charAt(index);
+      }, typingSpeed);
+  }
+
+  clear(delay: number) {
+    setTimeout(() => {
+      document.getElementById("typewriter")!.innerHTML = "> ";
+    }, delay);
+  }
+
+  delay(string: string): number {
+    return (string.length * this.typingSpeed) + 1500;
   }
 }
